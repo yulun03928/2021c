5c5a4a78eb788e5d46545c7a6cf8c2979ff61ed1
LI-YU-LUN <91250040+yulun03928@users.noreply.github.com>
2021-12-02 17:11:44 +0800

    Add files via upload

---
+++ week12-1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int a[10]={9,8,7,6,5,4,3,2,1,0};
+int main ()
+{
+    for(int i=0; i<10; i++) printf("%d ", a[i] );
+    printf("開始之前\n");
+
+    for(int i=0; i<10; i++)
+    {
+        for(int j=i+1; j<10; j++)
+        {
+            if( a[i] > a[j] )
+            {
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(int i=0; i<10; i++) printf("%d ", a[i] );
+    printf("最後結束\n");
+}
