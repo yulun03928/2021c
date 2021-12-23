19f5771c40265301e42b34c5e58049222e820a15
LI-YU-LUN <91250040+yulun03928@users.noreply.github.com>
2021-12-23 17:21:42 +0800

    Add files via upload

---
+++ week15/week15-3.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main ()
+{
+    int a,b;
+    scanf("%d %d", &a, &b);
+    int c=a%b;
+    printf("老大:%d 老二:%d 老三:%d\n", a, b, c);
+    while(1)
+    {
+        if(c==0)
+        {
+            printf("因為c是0,就break離開迴圈");
+            break;
+        }
+        a=b;
+        b=c;
+        c=a%b;
+        printf("老大:%d 老二:%d 老三:%d\n", a, b, c);
+    }
+    printf("離開迴圈後,答案是b: %d \n", b);
+}
